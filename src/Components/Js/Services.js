dbdb1196ca874a4cebbaf0bbbad6e6d70b3a0844
edfbad-Ex <68630287+edfbad-Ex@users.noreply.github.com>
2021-07-15 22:25:06 -0500

    Actualización

--- src/Components/Js/Services.js
+++ src/Components/Js/Services.js
@@ -4,58 +4,89 @@ import "../Css/Services.css";
 function Services() {
   return (
     <div className="Container-Services">
+
       <div className="Container-Services-Childs">
+        
         <div className="Container-Services-Child">
+        
           <div className="Container-Services-Child-Img"></div>
 
           <div className="Container-Services-Child-Text">
+        
             <span>Salas de tela y piel</span>
+        
           </div>
+        
         </div>
 
         <div className="Container-Services-Child">
+        
           <div className="Container-Services-Child-Img"></div>
 
           <div className="Container-Services-Child-Text">
+        
             <span>Alfombras</span>
+        
           </div>
+        
         </div>
 
         <div className="Container-Services-Child">
+        
           <div className="Container-Services-Child-Img"></div>
 
           <div className="Container-Services-Child-Text">
+        
             <span>Tapetes</span>
+        
           </div>
+        
         </div>
+      
       </div>
 
       <div className="Container-Services-Childs">
+      
         <div className="Container-Services-Child">
+      
           <div className="Container-Services-Child-Img"></div>
 
           <div className="Container-Services-Child-Text">
+      
             <span>Vestiduras de tela y piel</span>
+      
           </div>
+      
         </div>
 
         <div className="Container-Services-Child">
+      
           <div className="Container-Services-Child-Img"></div>
 
           <div className="Container-Services-Child-Text">
+      
             <span>Interiores de autobuses</span>
+      
           </div>
+      
         </div>
 
         <div className="Container-Services-Child">
+      
           <div className="Container-Services-Child-Img"></div>
 
           <div className="Container-Services-Child-Text">
+      
             <span>Sillas</span>
+      
           </div>
+      
         </div>
+     
       </div>
+   
     </div>
+    
   );
 }
 
